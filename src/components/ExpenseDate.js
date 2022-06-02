7cd840cec46cf93fc5dd868bba5d0be7c241b9d7
AnthonySeif0 <anthony.k.seif@gmail.com>
2022-06-02 13:16:17 +0300

    Date

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,14 @@
+import './ExpenseDate.css';
+
 function ExpenseDate({date}) {
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   const year = date.getFullYear();
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date_month">{month}</div>
+      <div className="expense-date_year">{year}</div>
+      <div className="expense-date_day">{day}</div>
     </div>
   );
 }
